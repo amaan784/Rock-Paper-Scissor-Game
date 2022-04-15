f7347817dee5d5516c73351658b4aef29dbf5ad6
Amaan Sheikh <amaan784@gmail.com>
2022-04-14 23:57:56 -0700

    in the works

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import random
 
+ROCK = 0
+PAPER = 1
+SCISSOR = 2
+
+
+class Game:
+    def __init__(self):
+        self.moves_history = []
+
 
 class Player:
     def __init__(self):
@@ -7,16 +16,25 @@ class Player:
         self.moves_history = []
         pass
 
+    def count_moves(self):
+        pass
 
-def decide():
+    def count_score(self):
+        self.score += 1
+
+
+def roll_hand():
     turn = random.randint(0, 2)
     return turn
 
 
 if __name__ == '__main__':
-    ROCK = 0
-    PAPER = 1
-    SCISSSOR = 2
+    player_1 = Player()
+    player_2 = Player()
+
+    while True:
+        pass
+
     play = decide()
     print(play)
 
